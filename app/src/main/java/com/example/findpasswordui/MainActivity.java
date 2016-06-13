77975f7fa9d021011afd2adf18371ec392449b58
Tony <tony@gmail.com>
2016-06-13 14:59:33 +0800

    修改了一下下

--- app/src/main/java/com/example/findpasswordui/MainActivity.java
+++ app/src/main/java/com/example/findpasswordui/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.findpasswordui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /******************测试代码*****************************/
     private Button button;
+    private Button open;
 
     /******************测试代码*****************************/
 
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         /******************测试代码*****************************/
         button=(Button)findViewById(R.id.button);
+        open = (Button)findViewById(R.id.open);
         button.setOnClickListener(this);
+        open.setOnClickListener(this);
+
 
         /******************测试代码*****************************/
     }
@@ -30,6 +34,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button:
                 Intent intent=new Intent("com.example.findpasswordui.ACTION_START");
                 startActivity(intent);
+                break;
+            case R.id.open:
+                Intent intent1 =new Intent(MainActivity.this,Open.class);
+                startActivity(intent1);
                 break;
             default:
                 break;
